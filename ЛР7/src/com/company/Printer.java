7f47d7021da1082d8ff337c3a1e694078b233e3f
alexproudmur <71935537+alexproudmur@users.noreply.github.com>
2021-02-22 10:23:50 +0200

    Delete ЛР7 directory

--- ЛР7/src/com/company/Printer.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface Printer {
-
-    void print(byte[] array);
-}
--- ЛР7/src/com/company/Sorter.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface Sorter {
-
-    byte[] sort(byte[] array);
-}
