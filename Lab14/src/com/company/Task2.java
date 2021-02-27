e5f988fa29f94b219ac9e7c80637d12a22ad1613
Alex <alexproudmur@gmail.com>
2021-02-27 14:25:20 +0200

    Изменена реализация метода задания 2

--- Lab14/src/com/company/Task2.java
+++ Lab14/src/com/company/Task2.java
@@ -4,38 +4,45 @@ import java.io.*;
 
 public class Task2 {
     public static void main (String[] args) {
-        lastShortestString("C:\\Users\\serg\\Documents\\lab14_part1.txt");
+        System.out.println(lastShortestString("C:\\Users\\alexp.DESKTOP-REM2UP4\\Documents\\lab14_part1.txt"));
     }
 
-    public static void lastShortestString(String filename) {
+    public static String lastShortestString(String filename) {
+
+        int lineNumber = 0; //номер текущий строки в файле
+        int minLineNumber = 0; //номер минимальной строчки в файле
+
+        String str; //текущая строка
+        String minString = null; //минимальная строка
+
+        int minLength = 1000000000; //инициализируем первоначальную минимальную длину очень большим числом
+        int tempLength; //временнная длина
+
 
         try (FileInputStream fis = new FileInputStream(filename);
              InputStreamReader isr = new InputStreamReader(fis);
              BufferedReader br = new BufferedReader(isr)) {
 
-            int lineNumber = 0;
-            String minStr = null;
-            int minLength = 10000;
-            int minlen2;
-
-            String str;
             while ((str = br.readLine()) != null) {
-                minlen2 = str.length();
-                if (minlen2 < minLength) {
-                    minLength = minlen2;
-                    minStr = str;
+                lineNumber++;
+                tempLength = str.length();
+
+                if (tempLength <= minLength) {
+                    minLength = tempLength;
+                    minString = str;
+                    minLineNumber = lineNumber;
                 }
-                System.out.println(++lineNumber + ": " + str +" ; " + str.length());
+
             }
 
-            System.out.println(minStr);
         } catch (FileNotFoundException ex) {
-            System.out.println("Sorry, no such file");
+            return ("Sorry, no such file");
 
         } catch (IOException ex) {
-            System.out.println("Oops, IO exception");
+            return ("Oops, IO exception");
         }
+
+        return "Стока # " + minLineNumber + " -- " + minString;
     }
 }
-
 
